9152a2861da377e5a3e00b95ac1fc79f4d03d361
MlodyJzB <cimochowski01@gmail.com>
2022-11-27 21:12:15 +0200

    Add comments to functions

--- OneLinkDoubleDirectionList/OneLinkDoubleDirectionList.cpp
+++ OneLinkDoubleDirectionList/OneLinkDoubleDirectionList.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <conio.h>
 
-// defines for actions
+// defines for actions switch
 #define ADD 1
 #define DEL 2
 #define FIND 3
@@ -52,6 +52,8 @@ int main() {
 	struct Node* prevFoundP = dumTailP;
 
 	while (1) {
+		// menu loop based od switch to run actions
+
 		printActions();
 		printf("\nChoose action: ");
 
@@ -143,16 +145,22 @@ int main() {
 }
 
 void initNode(float num, struct Node* nP) {
+	// init node
+
 	nP->number = num;
 	return;
 }
 
 void initDumNode(struct Node* nP) {
+	// init dummy node (head or tail)
+
 	nP->link = 0;
 	return;
 }
 
 void addN(struct Node* newNP, struct Node* dumHeadP, struct Node** prevHeadPP, struct Node** afterTailPP) {
+	// add provided node to the end of a list
+
 	newNP->link = dumHeadP - *prevHeadPP; // link to dummyHead
 
 	if (isDummy(*prevHeadPP)) {
@@ -168,10 +176,14 @@ void addN(struct Node* newNP, struct Node* dumHeadP, struct Node** prevHeadPP, s
 }
 
 bool isDummy(struct Node* nP) {
+	// check if node is dummy
+
 	return nP->link == 0;
 }
 
 struct Node* findN(float toFind, struct Node** prevDestPP, struct Node* dumTailP, struct Node* afterTailP) {
+	// find node by provided float and return it, provide parent node pointer of found node
+
 	struct Node* curP = afterTailP;
 	struct Node* prevCurP = dumTailP;
 	
@@ -185,6 +197,8 @@ struct Node* findN(float toFind, struct Node** prevDestPP, struct Node* dumTailP
 }
 
 void deleteN(struct Node* toDel, struct Node* prevNP, struct Node** prevHeadPP, struct Node** afterTailPP) {
+	// delete provided node, keeps list order, change prevHead and afterTail if is deleted
+	
 	struct Node* nextP = prevNP + toDel->link;
 
 	if (isDummy(nextP)) {
@@ -205,11 +219,15 @@ void deleteN(struct Node* toDel, struct Node* prevNP, struct Node** prevHeadPP, 
 }
 
 void editN(float num, struct Node* toEdit) {
+	// gives new float value to the provided node
+
 	toEdit->number = num;
 	return;
 }
 
 void dispT(struct Node* dumTailP, struct Node* afterTailP) {
+	// display whole list starting with afterTail node
+
 	struct Node* curP = afterTailP;
 	struct Node* prevCurP = dumTailP;
 
@@ -225,6 +243,8 @@ void dispT(struct Node* dumTailP, struct Node* afterTailP) {
 }
 
 void dispH(struct Node* dumHeadP, struct Node* prevHeadP) {
+	// display whole list starting with prevHead node
+
 	struct Node* curP = prevHeadP;
 	struct Node* prevCurP = dumHeadP;
 
@@ -240,6 +260,8 @@ void dispH(struct Node* dumHeadP, struct Node* prevHeadP) {
 }
 
 int getState() {
+	// ask for state number till valid (0-6)
+
 	int state;
 
 	do {
@@ -251,6 +273,8 @@ int getState() {
 }
 
 void printActions() {
+	// print possible actions
+
 	system("cls");
 	printf("Possible actions: \n1 - Add item\n2 - Delete item\n3 - Find item\n4 - Edit item\n5 - Show all items from beginning\n6 - Show all items from end\n0 - Exit\n");
 	return;
