b001a571f1c2c90d9e0ffd6bf0f54c03c674f1f0
MlodyJzB <cimochowski01@gmail.com>
2022-11-27 18:13:18 +0200

    Add initNode() and initDumNode()

--- OneLinkDoubleDirectionList/OneLinkDoubleDirectionList.cpp
+++ OneLinkDoubleDirectionList/OneLinkDoubleDirectionList.cpp
@@ -1,14 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Node {
 	float number;
 	int link;
 };
 
+void initNode(float num, struct Node* nP);
+
+void initDumNode(struct Node* nP);
+
 void addN(struct Node* newNP, struct Node* dumHeadP, struct Node** prevHeadPP, struct Node** afterTailPP);
 
 int main() {
+	struct Node* dumHeadP = (struct Node*)malloc(sizeof(struct Node));
+	initDumNode(dumHeadP);
 
+	struct Node* dumTailP = (struct Node*)malloc(sizeof(struct Node));
+	initDumNode(dumTailP);
+
+	struct Node* prevHeadP = dumTailP;
+	struct Node* afterTailP = dumHeadP;
+
+	
+}
+
+void initNode(float num, struct Node* nP) {
+	nP->number = num;
+}
+
+void initDumNode(struct Node* nP) {
+	nP->link = 0;
 }
 
 void addN(struct Node* newNP, struct Node* dumHeadP, struct Node** prevHeadPP, struct Node** afterTailPP) {
